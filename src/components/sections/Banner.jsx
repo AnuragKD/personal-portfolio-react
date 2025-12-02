2cdf04405619b8504520270f7697dafff83b7873
anurag <anurag95678@gmail.com>
2025-12-03 00:51:23 +0530

    added container and section component, added separating svg section

---
+++ src/components/sections/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../layout/container'
+import Section from '../layout/Section'
+
+const Banner = () => {
+  return (
+    <Section>
+        <Container>
+            <div>Banner</div>
+        </Container>
+    </Section>
+  )
+}
+
+export default Banner
