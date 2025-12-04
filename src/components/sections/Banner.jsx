41e17ce882153488fd37ff1b0523aff4ca71c0c7
anurag <anurag95678@gmail.com>
2025-12-05 00:04:05 +0530

    added primary font

--- src/components/sections/Banner.jsx
+++ src/components/sections/Banner.jsx
@@ -6,7 +6,11 @@ const Banner = () => {
   return (
     <Section>
         <Container>
-            <div>Banner</div>
+            <h1>Your startup deserves design that <br/>drives revenue</h1>
+            <p>We design products that are simple, intutive, and build to convert wether it's your first release or your next big revamb</p>
+            <button>
+              lets connect 
+            </button>
         </Container>
     </Section>
   )
